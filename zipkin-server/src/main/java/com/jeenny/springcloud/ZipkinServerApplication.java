36d002122e2e07d5d965041d24c280ebd2ae1862
lenny <524978009@qq.com>
2019-04-08 22:49:22 +0800

    zipkin-server

--- zipkin-server/src/main/java/com/jeenny/springcloud/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/jeenny/springcloud/ZipkinServerApplication.java
@@ -2,6 +2,8 @@ package com.jeenny.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
