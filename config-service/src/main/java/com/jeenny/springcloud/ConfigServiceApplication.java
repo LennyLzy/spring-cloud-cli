074ea3e3b0bacd915e3899cf0dde0e3df328f9a9
lenny <524978009@qq.com>
2019-04-11 17:32:46 +0800

    uaa-service

--- config-service/src/main/java/com/jeenny/springcloud/ConfigServiceApplication.java
+++ config-service/src/main/java/com/jeenny/springcloud/ConfigServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
+@RestController
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
--- gateway-server/src/main/java/com/jeenny/springcloud/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/jeenny/springcloud/GatewayServerApplication.java
@@ -1,17 +1,24 @@
 package com.jeenny.springcloud;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+//@EnableHystrix
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
@@ -31,5 +38,20 @@ public class GatewayServerApplication {
 	@GetMapping("/foo")
 	public String foo(){
 		return foo;
+	}
+
+	@RequestMapping(value = "/fallback")
+	public String fallback(){
+		return "error";
+	}
+
+	@RequestMapping("/timeout")
+	//放在别的服务请求，测试熔断，放在同一个网关服务里熔断不生效
+	public String timeout() throws InterruptedException{
+
+		//睡5秒，网关Hystrix3秒超时，会触发熔断降级操作
+		Thread.sleep(8000);
+
+		return "timeout";
 	}
 }
